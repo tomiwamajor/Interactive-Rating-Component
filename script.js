5cbcb4e96b1a54a3ee005502c3e9d59b2d031530
Adelekun Adetomiwa Kolapo <adetomiwaadelekun@gmail.com>
2023-09-06 11:51:45 +0100

    changes

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 "strict mode";
 
-const submitEl = document.querySelector(".rating-submit");
-const numbersEl = document.querySelectorAll(".numbers");
+const submit = document.querySelector(".rating-submit");
+let numbers = document.querySelectorAll(".numbers");
+let rated = document.querySelector("span");
 
-for (let i = 0; i < numbersEl.length; i++) {}
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function () {
+    console.log(`you selected ${i}`);
+    numbers[i].style.backgroundColor = "hsl(25, 97%, 53%)";
+    numbers[i].style.color = "hsl(0, 0%, 100%)";
 
-submitEl.addEventListener("click", function () {
-  console.log("Rating submitted");
+    if (`${i}` === "0") {
+      document.querySelector(".header").textContent = "You selected 1 out of 5";
+    } else if (`${i}` === "1") {
+      document.querySelector(".header").textContent = "You selected 2 out of 5";
+    } else if (`${i}` === "2") {
+      document.querySelector(".header").textContent = "You selected 3 out of 5";
+    } else if (`${i}` === "3") {
+      document.querySelector(".header").textContent = "You selected 4 out of 5";
+    } else if (`${i}` === "4") {
+      document.querySelector(".header").textContent = "You selected 5 out of 5";
+    }
+  });
+}
+
+submit.addEventListener("click", function () {
+  console.log("submit");
   document.querySelector(".rating").style.display = "none";
   document.getElementById("hidden").style.display = "flex";
   document.querySelector(".rating").classList.add("hidden");
+
+  // document.querySelector(".header").textContent = `You selected ${i} out of 5`;
 });
